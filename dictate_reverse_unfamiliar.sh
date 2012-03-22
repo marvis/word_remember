8b900f9c0a43aa9de116a5942dd53c9bda7696a0
xiaohang <hang.xiao.hh@gmail.com>
2012-03-22 01:58:25 -0400

    add dictate_reverse_unfamiliar.sh

--- dictate_reverse_unfamiliar.sh
+++ dictate_reverse_unfamiliar.sh
@@ -6,15 +6,15 @@ do
 	tran=`echo "$line" | awk -F \! '{print $2}'`
 	score1=`echo "$line" | awk -F \! '{print $3}'`
 	score2=`echo "$line" | awk -F \! '{print $4}'`
-	if [[ "$score1" -gt "0" ]];
+	if [[ $score2 -gt "0" ]];
 	then
 		say $word; read line; k=0; while [ "$line" != "$word" -a $k -lt 2 ]; do echo $tran ;say $word; read line; k=$[k+1]; done; echo "$word `dic $word | grep "\[.*\]" | head -n 1` $tran";
 		if [[ "$k" = "2" ]]
 		then 
-			score1=$[score1+1]
+			score2=$[score2+1]
 		elif [[ "$k" = "0" ]]
 		then
-			score1=$[score1-1]
+			score2=$[score2-1]
 		fi
 	fi
 	sed  "${i}s/^.*\$/$word!$tran!$score1!$score2/" unfamiliar  > .unfamiliar && mv .unfamiliar  unfamiliar
